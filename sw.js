8ff09806db0467b67264361b0485bb4d0cc7a499
evelyn-owl <alisa.golysheva.s@gmail.com>
2026-04-02 17:32:52 +0300

    Add PWA support: icon, manifest, service worker, install button

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'htoty-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.svg',
+  './manifest.json',
+  './data/animals.json',
+  './data/hp.json',
+  './data/peculiar.json',
+  './data/food.json',
+  './data/mythical.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
